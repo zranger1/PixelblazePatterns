9111a0d18534ec9babe64834b584483d5cd23239
zranger1 <35553094+zranger1@users.noreply.github.com>
2021-07-20 11:57:44 -0700

    Update matrixgreenwaterfall2d.js

--- matrixgreenwaterfall2d.js
+++ matrixgreenwaterfall2d.js
@@ -2,14 +2,19 @@
   Basic green "The Matrix"-style waterfall display adapted for 
   2D LED displays. 
   
-  Not a perfect reproduction, but extremely lightweight and fast enough
-  to be usable as a background or a texture in another pattern.
+  Not a perfect reproduction, but captures the feel pretty well,
+  and the code is small and fast enough to be usable as a background
+  or a texture in another pattern.
    
    7/15/21 JEM(zranger1)
 */
 
 var speed = 20;
 var hue = 0.3333;
+
+// You may want to adjust wavebase for seriously non-rectangular
+// displays.  Half the number of x axis pixels is a good place
+// to start.
 var waveBase = (sqrt(pixelCount) / 2);
 
 // uncomment mod() function for pixelblaze 2
