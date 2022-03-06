160387068bb49a90d05a70d6f894c7692859c1a5
zranger1 <35553094+zranger1@users.noreply.github.com>
2022-03-05 18:26:30 -0700

    A couple of new experimental things

---
+++ Experimental/circularities.js
@@ -0,0 +1,49 @@
+// Circularities, or the Rolling Donut  
+// MIT License
+//
+// 3/5/2022 ZRanger1
+
+var width = 0.05;
+
+function smoothstep(l,h,v) {
+    var t = clamp((v - l) / (h - l), 0.0, 1.0);
+    return t * t * (3.0 - 2.0 * t);
+}
+
+function circle(u,v,radius,cx,cy) {
+    var r = hypot(u-cx,v-cy);
+    r = abs(r-radius);
+    r = smoothstep(width+width,0, r);
+    return r;
+}
+
+
+function ringer(u,v) {
+  var a = 0;
+  var radius = 0.8;
+  cx = 0; cy = 0;
+  var dec = radius * 0.25;
+  
+  for (var i = 0; i < 4;i++) {
+    cx = dec * sin(2 * timebase + i+1);
+    cy = dec * cos(timebase+i+1);
+    a += circle(u,v,radius,cx,cy);
+    radius -= dec;
+  }
+  return a;
+}
+
+
+var timebase = 0;
+var t1;
+export function beforeRender(delta) {
+  timebase = (timebase + delta/1000) % 3600;
+  t1 = time(0.1);
+}
+
+translate(-0.5,-0.5);
+export function render2D(index,x,y) {
+  var hue = hypot(x,y);
+  var bri = ringer(x,y);
+  hsv(hue-t1,1,bri);
+}
---
+++ Experimental/strange-pinwheel2d.js
@@ -0,0 +1,27 @@
+// Weird colorful pinwheel thing.  Still figuring
+// out what to do with this...
+//
+// MIT License
+// 2/25/2022 ZRanger1
+
+export var speed = 1;
+
+export function sliderSpeed(v) {
+  speed = min(3,0.5 + v*3);
+}
+
+translate(-0.5,-0.5);
+scale(1.25,1.25);
+
+export function beforeRender(delta) {
+  timebase = time(0.15) * 10
+  t1 = timebase * speed;
+}
+
+export function render2D(index,x,y) {
+  d = abs(frac((x*x + y*y) - t1));  // waves
+  a = abs(atan2(x,y)-t1) % (2-wave(time(0.2)));  // rays
+  
+  b = max(a,d); 
+  hsv(b-d,1,b*b*b)
+}
