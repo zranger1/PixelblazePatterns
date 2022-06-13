d87646bdda7ca2385e12e76cf12d3c2be3fcca19
ZRanger1 <35553094+zranger1@users.noreply.github.com>
2022-06-12 19:46:33 -0700

    Add boxplosion2D and follow-that-star-2D

---
+++ Experimental/boxplosion2D.js
@@ -0,0 +1,40 @@
+// Boxplosion 2D - An explosion of brightly colored boxes!
+//
+// MIT License
+// Take this code and use it to make cool things!
+//
+// 6/12/2022 ZRanger1
+//
+
+var iterations = 3;
+var timebase = 0;
+var t1,t2,t3;
+
+translate(-0.5,-0.5);
+scale(4,4)
+export function beforeRender(delta) {
+  timebase = (timebase + delta/1000) % 3600;
+  
+  t1 = timebase * 2;
+  t2 = mod(t1,2); t2 = t2*t2*t2;
+  t3 = 0.05 * abs(sin(t1*2));
+}
+
+export function render2D(index,u,v) {
+  var a = 1;
+  
+  // all the sub-boxes are always present -- they just move
+  // apart as a function of sin(time).
+  for (var x = 0; x < iterations; ++x)     {
+    for (var y = 0; y < iterations; ++y) {
+      cx = (x / (iterations-1) - 0.5) * 2;
+      cy = (y / (iterations-1) - 0.5) * 2;
+      px = 0.5 * cx * t2;
+      py = 0.5 * cy * t2;
+       
+      a = min(a, max(abs(u-px)-t3,abs(v-py)-t3));
+    }
+  }
+  a = 1-clamp(a,0,1);
+  hsv(a,1,a);
+}
---
+++ Experimental/follow-that-star-2D.js
@@ -0,0 +1,57 @@
+// Follow that Star! 2D
+//
+// Still kind of experimental at this point.
+//
+// Started life as a frame buffer-less snake pattern, then I started
+// playing around with Minkowski distance at fractional powers and...
+//
+// MIT License
+// Take this code and use it to make cool things!
+// 6/12/2022 ZRanger1
+//
+
+var snakeSize = 0.7;
+var segSpacing = 1;
+var segments = 4;
+var speed = 1.8;
+
+var timebase = 0;
+var t1,t2;
+var xoffs = array(segments);
+var yoffs = array(segments)
+
+
+translate(-0.5,-0.5); 
+scale(2,2)
+
+export function beforeRender(delta) {
+  timebase = (timebase + delta / 1000) % 3600;
+  t1 = timebase * speed;
+  t2 = time(0.1);
+  
+  // precalculate distance offsets for each segment
+  for (i = 0;i < segments; i++ ) {
+    var t = t1 - segSpacing * (i+1);     
+     xoffs[i] = 0.92 * sin(t) + 0.05 * cos(t * 6);
+     yoffs[i] = 0.65 * sin(t * 0.85) + 0.13 * sin(t * 2);     
+  }
+  
+  
+}
+
+function minkowskiDistance(x1,y1,p) {
+  return pow(pow(abs(x1), p) + pow(abs(y1), p),1.0 / p);
+}
+
+export function render2D(index,x,y) {
+  var b = 0;
+
+  for (i = 0; i < segments; i++) {
+    var px = xoffs[i];
+    var py = yoffs[i];
+
+    b += snakeSize / minkowskiDistance(x-px,y-py,0.375) / (i+1);
+  }
+
+  hsv(t2,2.5-b,b*b);
+}
